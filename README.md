c0d2ed13769b2900db88b8902fd86b21e2a92de0
Hateman31 <metilogrunge31@gmail.com>
2016-07-19 09:03:29 +0300

    material design

--- README.md
+++ README.md
@@ -7,3 +7,6 @@
 * [С места в карьер](./lessons/lesson1.md)
 * [Делаем калькулятор](./lessons/lesson2.md)
 * [Немного о трюках](./tricks_1.md)
+
+#####Примеры:
+* [Меню в стиле material](./design/material/)
---
+++ design/material/test.py
@@ -0,0 +1,14 @@
+import kivy
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+
+
+class Test(BoxLayout):
+	pass
+	
+class TestApp(App):
+	def build(self):
+		return Test()
+		
+TestApp().run()
+gi
